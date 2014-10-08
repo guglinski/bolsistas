8e875a009ef65926e4b378a8aafa26cd009f596a
victorreis <v.crisostomo.reis@gmail.com>
2014-10-07 21:09:02 -0300

    Esboço do Layout. Victor Reis

--- site/index.php
+++ site/index.php
@@ -1,20 +1,44 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="./styles/styles.css">
+        <title>Trainee Manager</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
-        teste
-        teste
-        teste Dimitri
+        <div id="topo">
+            <div id="logo">
+                Trainee Manager
+            </div>
+            <div id="search">
+                
+            </div>
+        </div>
+        <div id="menu">
+            <div id="menuNav">
+                <div class="linkMenu"><a href="#">Bolsista</a></div>
+                <div class="linkMenu"><a href="#">Projeto</a></div>
+                <div class="linkMenu"><a href="#">Contato</a></div>
+            </div>
+        </div>
+        <div id="bigBox">
+            <div id="content">
+                
+            </div>
+            <div id="login">
+                <form name="login" action="login.php" method="POST" enctype="multipart/form-data">
+                    Login:<br><input type="text" name="login" value="" />
+                    Senha:<input type="text" name="password" value="" />
+                    <input type="submit" value="Entrar" name="entrar" />
+                </form>
+                
+            </div>
+        </div>
+        <div id="line">
+            
+        </div>
+        <div id="footer">
+            
+        </div>
     </body>
 </html>
